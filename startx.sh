ef512ecf900ceb1d97d58023a0c27212024685c5
Micah Scott <micah@scanlime.org>
2023-01-22 10:47:33 -0800

    clean exit

--- startx.sh
+++ startx.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+trap "
+  pkill -f /home/user/.banner
+  pkill tor websockify Xvnc
+" EXIT
+
 tor -f /etc/tor/torrc &
 xinit &
 websockify --web=/usr/share/novnc localhost:8000 localhost:5900 &
+
 wait
